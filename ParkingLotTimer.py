6bdb2ac71c3766d87eed257c7e0b0b5858ac9e51
David Kalel Sta.Teresa <23-00401@g.batstate-u.edu.ph>
2024-04-07 00:30:45 +0800

    Update1

---
+++ ParkingLotTimer.py
@@ -0,0 +1,17 @@
+parking_lot_list = {
+    1:{"Slot" : "1"},
+    2:{"Slot" : "2"},
+    3:{"Slot" : "3"},
+    4:{"Slot" : "4"},
+    5:{"Slot" : "5"},
+    6:{"Slot" : "6"},
+    7:{"Slot" : "7"},
+    8:{"Slot" : "8"},
+    9:{"Slot" : "9"},
+    10:{"Slot" : "10"},
+}
+
+user_info={}
+
+def sign_up():
+    print()
